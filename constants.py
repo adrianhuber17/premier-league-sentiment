3a20d039819ad8d674237b504fb0f951822a3e20
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-13 16:19:24 -0800

    tweet count fetch completed for premier league teams

---
+++ constants.py
@@ -0,0 +1,4 @@
+premier_league_teams = ["arsenal","aston villa","bournemouth","brentford fc","brighton hove albion","chelsea",
+                        "crystal palace","everton","fulham","leeds united","leicester city","liverpool","manchester city",
+                        "manchester united","newcastle","nottingham forest","southampton","tottenham",
+                        "west ham","wolverhampton wanderers"]
--- get_tweet_count.py
+++ get_tweet_count.py
@@ -2,15 +2,12 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+from constants import premier_league_teams
 
 load_dotenv()
 bearer_token = os.getenv('BEARER_TOKEN')
 
 search_url = "https://api.twitter.com/2/tweets/counts/recent"
-
-# Optional params: start_time,end_time,since_id,until_id,next_token,granularity
-query_params = {'query': 'chelsea fc','start_time': '2022-11-07T00:00:00.000Z'}
-
 
 def bearer_oauth(r):
     """
@@ -24,16 +21,27 @@ def bearer_oauth(r):
 
 def connect_to_endpoint(search_url, params):
     response = requests.request("GET", search_url, auth=bearer_oauth, params=params)
-    print(response.status_code)
+    # print(response.status_code)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
 
 
-def main():
-    json_response = connect_to_endpoint(search_url, query_params)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+def get_team_tweet_count():
+    team_tweets_count = {}
+    for team in premier_league_teams:
+        query_params = {'query': team,'start_time': '2022-11-08T00:00:00.000Z'}
+        json_response = connect_to_endpoint(search_url, query_params)
+        total_tweet_count = json_response["meta"]
+        team_tweets_count[team] = total_tweet_count
+        # print(json.dumps(json_response, indent=4, sort_keys=True))
+    return team_tweets_count
 
 
-if __name__ == "__main__":
-    main()
+team_tweets_count = get_team_tweet_count()
+rank = []
+for team,total_tweet_count in team_tweets_count.items():
+    rank.append((team,total_tweet_count['total_tweet_count']))
+
+rank.sort(key=lambda y:y[1],reverse=True)
+print(rank)
