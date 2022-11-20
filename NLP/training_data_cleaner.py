3f2b62d3db53962575be75bacc7d5c12d1631956
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-19 16:35:04 -0800

    created file to clean training data, and created file to train and save model

--- NLP/training_data_cleaner.py
+++ NLP/training_data_cleaner.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # nltk :
 import nltk
-from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer,PorterStemmer
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -29,7 +29,7 @@ data = pd.read_csv('NLP/training_data_set.csv', encoding =DATASET_ENCODING , nam
 print("---importing training data-----")
 print(data.head())
 print("-----changin target from 4 to 1 for positive comments")
-X = data.iloc[:,[5]]
+# X = data.iloc[:,[5]]
 Y = data.iloc[:,0]
 Y[Y == 4] = 1
 print(data.head())
@@ -71,62 +71,118 @@ print(data.groupby('target').count())
 # Text-preprocessing
 print("-----text processing-----")
 # Missing Values
-num_missing_desc = data.isnull().sum()[2]    # No. of values with msising descriptions
-print('Number of missing values: ' + str(num_missing_desc))
-data = data.dropna()
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
 TAG_CLEANING_RE = "@\S+"
 print("-----removing @tags-----")
 # Remove @tags
-X['text'] = X['text'].map(lambda x: re.sub(TAG_CLEANING_RE, ' ', x))
+data['text'] = data['text'].apply(lambda x: re.sub(TAG_CLEANING_RE, ' ', x))
+
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
 # Smart lowercase
 print("-----making lower case-----")
-X['text'] = X['text'].map(lambda x: x.lower())
+data['text'] = data['text'].str.lower()
+
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
 # Remove numbers
 print("-----removing number-----")
-X['text'] = X['text'].map(lambda x: re.sub(r'\d+', ' ', x))
+def cleaning_numbers(data):
+    return re.sub('[0-9]+', '', data)
+data['text'] = data['text'].apply(lambda x: cleaning_numbers(x))
+
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
 # Remove links
 print("-----removing links-----")
-TEXT_CLEANING_RE = "https?:\S+|http?:\S|[^A-Za-z0-9]+"
-X['text'] = X['text'].map(lambda x: re.sub(TEXT_CLEANING_RE, ' ', x))
+def cleaning_URLs(data):
+    return re.sub('((www.[^s]+)|(https?://[^s]+))', ' ', data)
+data['text'] = data['text'].apply(lambda x: cleaning_URLs(x))
+
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
 # Remove Punctuation
 print("-----removing punctuation-----")
-X['text']  = X['text'].map(lambda x: x.translate(x.maketrans('', '', string.punctuation)))
+english_punctuation_list = string.punctuation
+def cleaning_punctuations(text):
+    translator = str.maketrans('', '', english_punctuation_list)
+    return text.translate(translator)
+data['text'] = data['text'].apply(lambda x: cleaning_punctuations(x))
 
-# Remove white spaces
-print("-----removing white spaces-----")
-X['text'] = X['text'].map(lambda x: x.strip())
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
-# Tokenize into words
-print("-----tokenizing into words-----")
-X['text'] = X['text'].map(lambda x: word_tokenize(x))
- 
-# Remove non alphabetic tokens
-print("-----removing non alphabetic tokens-----")
-X['text'] = X['text'].map(lambda x: [word for word in x if word.isalpha()])
+#Cleaning and removing repeating characters
+print("-----removing repeating characters-----")
+def cleaning_repeating_char(text):
+    return re.sub(r'(.)1+', r'1', text)
+data['text'] = data['text'].apply(lambda x: cleaning_repeating_char(x))
+
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
 # Filter out stop words
 print("-----filter out stop words-----")
 stop_words = set(stopwords.words('english'))
-X['text'] = X['text'].map(lambda x: [w for w in x if not w in stop_words])
-    
-# Word Lemmatization
-print("-----Word lemmentization-----")
-lem = WordNetLemmatizer()
-X['text'] = X['text'].map(lambda x: [lem.lemmatize(word,"v") for word in x])
+def cleaning_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in stop_words])
+data['text'] = data['text'].apply(lambda text: cleaning_stopwords(text))
 
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
+
+# Remove white spaces
+print("-----removing white spaces-----")
+data['text'] = data['text'].apply(lambda x: x.strip())
+
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
+
+# Tokenize into words
+print("-----tokenizing into words-----")
+data['text'] = data['text'].apply(word_tokenize)
+
+# Applying Stemming
+print("-----Applying Stemming-----")
+st = PorterStemmer()
+def stemming_on_text(data):
+    text = [st.stem(word) for word in data]
+    return data
+data['text'] = data['text'].apply(lambda x: stemming_on_text(x))
+
+# ----Applying Lemmatizer :----
+print("-----Applying Lemmatizer-----")
+lm = WordNetLemmatizer()
+def lemmatizer_on_text(data):
+    text = [lm.lemmatize(word) for word in data]
+    return data
+data['text'] = data['text'].apply(lambda x: lemmatizer_on_text(x))
+ 
 # Turn lists back to string
 print("-----Turn tokenized list back to a string-----")
-X['text'] = X['text'].map(lambda x: ' '.join(x))
+data['text'] = data['text'].apply(lambda x: ' '.join(x))
 
-print("-----Checking clean text column-----")
-print(X.head())
+# Missing Values
+check_nan = data['text'].isnull().values.any()
+print('missing values?: ' + str(check_nan))
 
+print("-----Checking data-----")
+data = data.dropna()
+print(data.head())
 #send clean data to csv
-print("-----sending clean data and target to csv-----")
-X.to_csv("NLP/clean_dataset.csv")
-Y.to_csv("NLP/clean_target.csv")
+print("-----sending clean data to csv-----")
+data.to_csv("NLP/clean_dataset.csv",na_rep='NULL')
--- NLP/training_model.py
+++ NLP/training_model.py
@@ -6,9 +6,98 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
 
-#pandas:
+# plotting :
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#utilities:
 import pandas as pd
+import numpy as np
+import time
 
 # -----importing clean training data-----
 print("-----importing clean training data-----")
-x_data = pd.read_csv()
+DATASET_ENCODING = "ISO-8859-1"
+DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
+data = pd.read_csv('NLP/clean_dataset.csv',encoding=DATASET_ENCODING,names=DATASET_COLUMNS,low_memory=False)
+# y_data = pd.read_csv('NLP/clean_target.csv',encoding=DATASET_ENCODING)
+print("-----Traning dataset null values-----")
+check_nan = data['text'].isnull().values.any()
+print("missing values total: ",np.sum(data.isnull().any(axis=1)))
+print('missing values?: ' + str(check_nan))
+data = data.dropna()
+check_nan = data['text'].isnull().values.any()
+
+print("-----Remove dataset null values-----")
+print('missing values?: ' + str(check_nan))
+print("missing values total: ",np.sum(data.isnull().any(axis=1)))
+
+# ----Separating input feature and label :----
+print("-----creating training and test data-----")
+X = data['text']
+y = data['target']
+
+
+# -----Separating the 95% data for training data and 5% for testing data-----
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=26105111)
+print("TRAIN size:", len(X_train))
+print("TEST size:", len(X_test))
+
+# -----Fit the TF-IDF Vectorizer----- 
+print("-----Fit the TF-IDF Vectorizer-----")
+vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+vectoriser.fit(X_train)
+print('No. of feature_words: ', len(vectoriser.get_feature_names()))
+
+# -----Transform the data using TF-IDF Vectorizer-----
+print("-----Transform the data using TF-IDF Vectorizer-----")
+X_train = vectoriser.transform(X_train)
+X_test  = vectoriser.transform(X_test)
+
+def model_evaluate(model):
+    # Predict values for Test dataset
+    y_pred = model.predict(X_test)
+    #model test score
+    print("-----model test score-----")
+    # Print the evaluation metrics for the dataset.
+    print(classification_report(y_test, y_pred))
+    # Compute and plot the Confusion matrix
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    categories = ['Negative','Positive']
+    group_names = ['True Neg','False Pos', 'False Neg','True Pos']
+    group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+    labels = [f'{v1}n{v2}' for v1, v2 in zip(group_names,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
+    xticklabels = categories, yticklabels = categories)
+    plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
+    plt.ylabel("Actual values" , fontdict = {'size':14}, labelpad = 10)
+    plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+    plt.show()
+
+# Model-1 : Bernoulli Naive Bayes.
+print("-----creating and training Bernoulli Naive Bayes Model-----")
+BNBmodel = BernoulliNB()
+start = time.time()
+BNBmodel.fit(X_train, y_train)
+end = time.time()
+print("The execution time of this model is {:.2f} seconds\n".format(end-start))
+print("-----evaluating model-----")
+model_evaluate(BNBmodel)
+y_pred1 = BNBmodel.predict(X_test)
+print('test_predictions: ',y_pred1)
+print("model score: ",BNBmodel.score(X_test,y_test))
+
+
+#saving model
+print("-----saving model-----")
+import pickle
+file_name = 'NLP/bernoulli_model.sav'
+pickle.dump(BNBmodel,open(file_name,'wb'))
+
+#TODO: send loaded model to a different file to process football tweets
+#load model
+# loaded_bernoulli_model = pickle.load(open(file_name,'rb'))
+# y_pred1 = loaded_bernoulli_model.predict(X_test)
+# print(y_pred1)
+# print(loaded_bernoulli_model.score(X_test,y_test))
