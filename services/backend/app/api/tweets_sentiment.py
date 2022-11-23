ff7799c04f310cb0c509e0b38657e8ed179cdb4c
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-23 15:14:21 -0800

    change get sentiment naming convention. added post request, but need to work on it

--- services/backend/app/api/tweets_sentiment.py
+++ services/backend/app/api/tweets_sentiment.py
@@ -2,12 +2,14 @@ from flask_restx import Namespace,Resource
 from app.api.crud import get_tweet_sentiment
 from flask import jsonify
 
-get_tweet_namespace = Namespace("get-tweets")
+get_tweet_sentiment_namespace = Namespace("get-tweets")
 
-class GetTweets(Resource):
+class GetTweetsSentiment(Resource):
     def get(self):
         tweet_sentiment_json = get_tweet_sentiment()
-        print(tweet_sentiment_json)
         return jsonify({"status":"success","message":tweet_sentiment_json})
+    
+    def post(self):
+        return
 
-get_tweet_namespace.add_resource(GetTweets,"")
+get_tweet_sentiment_namespace.add_resource(GetTweetsSentiment,"")
