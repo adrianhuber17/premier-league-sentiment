7b034a6016f4746be6f63a3547852cb5d17019ef
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-23 12:03:07 -0800

    changed csv location paths

--- services/backend/app/NLP/train_create_model.sh
+++ services/backend/app/NLP/train_create_model.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 chmod +x train_create_model.sh
-source env/bin/activate
 echo '-----cleaning training data-----'
-python3 -m services.NLP.train_model.training_data_cleaner
+python3 -m train_model.training_data_cleaner
 echo '-----data is clean and ready for training model-----'
-python3 -m services.NLP.train_model.training_model
+python3 -m train_model.training_model
 echo '-----sentiment model created-----'
 
