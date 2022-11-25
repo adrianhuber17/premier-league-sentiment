0631fb64a099eb99e97989c93b4f264b4a6b952b
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-25 15:33:27 -0800

    updated constants for testing

--- services/backend/app/helper/constants.py
+++ services/backend/app/helper/constants.py
@@ -1,6 +1,6 @@
-PREMIER_LEAGUE_TEAMS = ["arsenal fc","chelsea fc"]
-
 # PREMIER_LEAGUE_TEAMS = ["arsenal fc","aston villa","bournemouth","brentford fc","brighton hove albion","chelsea fc",
 #                         "crystal palace","everton","fulham fc","leeds united","leicester city","liverpool fc","manchester city",
 #                         "manchester united","newcastle fc","nottingham forest","southampton","tottenham",
 #                         "west ham","wolverhampton wanderers"]
+
+PREMIER_LEAGUE_TEAMS = ["arsenal fc","manchester united"]
