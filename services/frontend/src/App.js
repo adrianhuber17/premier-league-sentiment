6bcd8170f4f5a464a29db193b2657f1738b2ee0b
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-29 13:36:14 -0800

    created first front end component with chart.js to start setting up the plot for sentiment

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,9 +1,13 @@
+import { SentimentPlot } from "./components/SentimentPlot";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>NLP Football Sentiment App... more to come</h1>
+      <>
+        <SentimentPlot />
+      </>
     </div>
   );
 }
