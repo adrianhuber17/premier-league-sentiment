bebe7bf1df24908de7f9a055472c82034d5865bb
adrianhuber17 <adrianhuber17@gmail.com>
2022-11-30 10:27:07 -0800

    updated sentimentplot function and added comment to include a dynamic date for the previous 7 days

--- services/frontend/src/components/SentimentPlot.jsx
+++ services/frontend/src/components/SentimentPlot.jsx
@@ -30,32 +30,35 @@ export const options = {
     },
     title: {
       display: true,
-      text: "Chart.js Line Chart",
+      text: "Premier League Sentiment",
     },
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
-
 export function SentimentPlot({ sentiment }) {
+  //labels need to be dynamic for the current day minus the 7 previous days
+  const labels = ["November", "December"];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Arsenal FC",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Chelsea FC",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
   console.log(sentiment);
   return <Line options={options} data={data} />;
 }
