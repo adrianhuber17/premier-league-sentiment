46a3ee1112da521b1fdd4327dbf10275de2a69a5
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-02 10:46:57 -0800

    added team colors for chart and also added loop to create dataset for plotting

--- services/frontend/src/components/SentimentPlot.jsx
+++ services/frontend/src/components/SentimentPlot.jsx
@@ -10,7 +10,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
 ChartJS.register(
   CategoryScale,
@@ -36,31 +36,73 @@ export const options = {
 };
 
 export function SentimentPlot({ sentiment }) {
-  //labels need to be dynamic for the current day minus the 7 previous days
+  const teamBorderColors = {
+    "chelsea fc": "rgba(3, 70, 148)",
+    "manchester city": "rgba(108, 171, 221)",
+    "arsenal fc": "rgba(239, 1, 7)",
+    "aston villa": "rgba(149,191,229)",
+    bournemouth: "rgba(218, 41, 28)",
+    "brighton hove albion": "rgba(0, 87, 184)",
+    "crystal palace": "rgba(27, 69, 143)",
+    everton: "rgba(39,68,136)",
+    "fulham fc": "rgba(0, 0, 0)",
+    "leeds united": "rgba(255,205,0)",
+    "leicester city": "rgba(0,83,160)",
+    "liverpool fc": "rgba(200, 16, 46)",
+    "manchester united": "rgba(218, 41, 28)",
+    "newcastle fc": "rgba(45, 41, 38)",
+    "nottingham forest": "rgba(229, 50, 51)",
+    "southampton fc": "rgba(215, 25, 32)",
+    tottenham: "rgba(19, 34, 87)",
+    "west ham": "rgba(122, 38, 58)",
+    "wolverhampton wanderers": "rgba(253,185,19)",
+  };
+  const teamBackgroundColors = {
+    "manchester united": "rgba(218, 41, 28,0.5)",
+    "chelsea fc": "rgba(3, 70, 148, 0.5)",
+    "manchester city": "rgba(108, 171, 221,0.5)",
+    "arsenal fc": "rgba(239, 1, 7,0.5)",
+    "aston villa": "rgba(149,191,229,0.5)",
+    bournemouth: "rgba(218, 41, 28,0.5)",
+    "brighton hove albion": "rgba(0, 87, 184,0.5)",
+    "crystal palace": "rgba(27, 69, 143,0.5)",
+    everton: "rgba(39,68,136,0.5)",
+    "fulham fc": "rgba(0, 0, 0,0.5)",
+    "leeds united": "rgba(255,205,0,0.5)",
+    "leicester city": "rgba(0,83,160,0.5)",
+    "liverpool fc": "rgba(200, 16, 46,0.5)",
+    "newcastle fc": "rgba(45, 41, 38,0.5)",
+    "nottingham forest": "rgba(229, 50, 51,0.5)",
+    "southampton fc": "rgba(215, 25, 32,0.5)",
+    tottenham: "rgba(19, 34, 87,0.5)",
+    "west ham": "rgba(122, 38, 58,0.5)",
+    "wolverhampton wanderers": "rgba(253,185,19,0.5)",
+  };
 
+  const datasets = [];
+  Object.keys(sentiment).forEach(function (key) {
+    //   console.log(key);
+    //   console.log(teams[key]);
+    const datasetsObj = {};
+    datasetsObj["label"] = key;
+    datasetsObj["data"] = sentiment[key];
+    datasetsObj["lineTension"] = 0.1;
+    if (key in teamBorderColors) {
+      datasetsObj["borderColor"] = teamBorderColors[key];
+    } else {
+      datasetsObj["borderColor"] = "rgb(255, 99, 132)";
+    }
+    if (key in teamBackgroundColors) {
+      datasetsObj["backgroundColor"] = teamBackgroundColors[key];
+    } else {
+      datasetsObj["backgroundColor"] = "rgba(255, 99, 132, 0.5)";
+    }
+    datasets.push(datasetsObj);
+  });
   const labels = ["11/29/2022", "11/30/2022", "11/31/2022"];
   const data = {
     labels,
-    datasets: [
-      {
-        label: "arsenal fc",
-        lineTension: 0.1,
-        data: labels.map(() =>
-          faker.datatype.number({ min: -1000, max: 1000 })
-        ),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        label: "chelsea fc",
-        lineTension: 0.1,
-        data: labels.map(() =>
-          faker.datatype.number({ min: -1000, max: 1000 })
-        ),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
+    datasets,
   };
 
   console.log(sentiment);
