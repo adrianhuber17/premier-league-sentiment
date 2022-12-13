788a49a9645e91bb846572988231f32c20063aad
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-12 20:03:55 -0800

    added testing for get-tweet-count route

--- services/backend/app/tests/test_example.py
+++ services/backend/app/tests/test_example.py
@@ -3,13 +3,12 @@ from app.helper.constants import DATABASE_INJECTION_2
 from app import db
 from app.api.model import TweetCount
 
-def test_get_tweet_count(test_app,test_database):
-    tweet_count = TweetCount(date_start=DATABASE_INJECTION_2[-1],tweet_count=DATABASE_INJECTION_2[0])
-    db.session.add(tweet_count)
-    db.session.commit()
+
+def test_get_tweet_count_empty_db(test_app,test_database):
+
     client = test_app.test_client()
     resp = client.get("/get-tweet-count")
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
     assert data["status"] == "success"
-    assert data["message"]["start_date"] == "Sat, 26 Nov 2022 22:53:34 GMT"
+    assert data["message"] == {}
