7b9f076c46bd4e3f1dcb72d7bd42d8cff39aba0d
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-15 19:39:09 -0800

    added comments to testing

--- services/backend/app/tests/test_tweet_count.py
+++ services/backend/app/tests/test_tweet_count.py
@@ -4,6 +4,7 @@ from app import db
 from app.api.model import TweetCount
 
 def test_get_tweet_count(test_app,test_database):
+    """testing /get-tweet-count with a normal database injection"""
     tweet_count = TweetCount(date_start=DATABASE_INJECTION_2[-1],tweet_count=DATABASE_INJECTION_2[0])
     db.session.add(tweet_count)
     db.session.commit()
@@ -15,7 +16,7 @@ def test_get_tweet_count(test_app,test_database):
     assert data["message"]["start_date"] == "Sat, 26 Nov 2022 22:53:34 GMT"
 
 def test_get_tweet_count_empty_db(test_app,test_database):
-
+    """testing /get-tweet-count with an empy database"""
     client = test_app.test_client()
     resp = client.get("/get-tweet-count")
     data = json.loads(resp.data.decode())
