c6f4da088275677cc9ec5d5f04ef24b8ddc1991d
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-15 19:39:55 -0800

    added comments to testing

--- services/backend/app/tests/test_tweet_sentiment.py
+++ services/backend/app/tests/test_tweet_sentiment.py
@@ -4,6 +4,7 @@ from app import db
 from app.api.model import Tweet
 
 def test_get_tweet_sentiment(test_app,test_database):
+    """testing /get-sentiment with a normal database injection"""
     tweet_sentiment = Tweet(tweets = DATABASE_INJECTION)
     db.session.add(tweet_sentiment)
     db.session.commit()
@@ -16,7 +17,7 @@ def test_get_tweet_sentiment(test_app,test_database):
     assert "arsenal" in data["message"][0]
 
 def test_get_tweet_count_empty_db(test_app,test_database):
-
+    """testing /get-sentiment with am empy database injection"""
     client = test_app.test_client()
     resp = client.get("/get-sentiment")
     data = json.loads(resp.data.decode())
