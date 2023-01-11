1fefce802dcf1908ecd2386209745a69c10b17d6
adrianhuber17 <adrianhuber17@gmail.com>
2023-01-10 21:11:58 -0800

    gathered imports for data analysis

--- data_analysis/sampling/nlp_small_sample.py
+++ data_analysis/sampling/nlp_small_sample.py
@@ -1,3 +1,24 @@
+## imports ##
+# utilities : 
+import re 
+import numpy as np
+import pandas as pd
+import string
+
+# plotting :
+# import seaborn as sns
+# from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# nltk :
+import nltk
+from nltk.stem import WordNetLemmatizer,PorterStemmer
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 # get new data
 # analyze new data
 # clean new data
