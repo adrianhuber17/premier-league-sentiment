0cc2243c77aaa23e06cd903583b029adb81e8b43
adrianhuber17 <adrianhuber17@gmail.com>
2023-01-12 18:06:40 -0800

    split data into two df for positive and negative sentiment

--- data_analysis/sampling/nlp_small_sample.py
+++ data_analysis/sampling/nlp_small_sample.py
@@ -11,21 +11,29 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # nltk :
-import nltk
-from nltk.stem import WordNetLemmatizer,PorterStemmer
-from nltk.tokenize import word_tokenize
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+# import nltk
+# from nltk.stem import WordNetLemmatizer,PorterStemmer
+# from nltk.tokenize import word_tokenize
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+# nltk.download('stopwords')
+# from nltk.corpus import stopwords
 
 # import new data
 columns = ["target","number","date","query","user","text"]
 df = pd.read_csv("/Users/adrianhuber/football-sentiment/services/backend/app/NLP/train_model/training_data_set.csv",encoding_errors='ignore')
 df.columns = columns
+
+# filtered data for text and target
 raw_data_df = df[["target","text"]]
-print(raw_data_df)
+
+# df for negative and positive data
+neg_df = raw_data_df.loc[raw_data_df["target"]==4]
+pos_df = raw_data_df.loc[raw_data_df["target"]==0]
+
+print(neg_df)
+print(pos_df)
 # analyze new data
 # clean new data
 # separate training data from test data
